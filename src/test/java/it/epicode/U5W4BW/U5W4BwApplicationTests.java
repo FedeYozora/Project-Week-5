a6eaee32143c7a847ad1a7017ab394126c801534
FedeYozora <federicobonfi@gmail.com>
2024-02-26 11:37:29 +0100

    first commit

---
+++ src/test/java/it/epicode/U5W4BW/U5W4BwApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.U5W4BW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W4BwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
