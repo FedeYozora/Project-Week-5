47de68b076331169547fb2c13c2d936b2e051d55
Otman Karim <71219931+Ott00@users.noreply.github.com>
2024-02-27 15:05:30 +0100

    Edit in validation for Role DTO

--- src/main/java/it/epicode/U5W4BW/payloads/UserRoleDTO.java
+++ src/main/java/it/epicode/U5W4BW/payloads/UserRoleDTO.java
@@ -1,9 +1,11 @@
 package it.epicode.U5W4BW.payloads;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserRoleDTO(
         @NotBlank(message = "Role cannot be blank")
+        @Size(min = 3, message = "Role must be at least 3 characters")
         String role
 ) {
 }
